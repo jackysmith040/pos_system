f55be41764ac69f95d6ca6a029f0c362f2be8898
jacky040 <116127513+jackysmith040@users.noreply.github.com>
2024-11-06 13:53:25 +0000

    added small style

--- pos/footer.php
+++ pos/footer.php
@@ -1,11 +1,11 @@
 <style>
-    <?php include 'footer.css';?>
+    <?php include 'footer.css'; ?>
 </style>
 
 <footer class="main-footer">
     <div class="footer-content">
         <p>
-            <strong>&copy; 2024 <a href="#">DonzyTech</a></strong>
+            <strong>&copy; 2024 <a href="#">Donzy<span class="text-tech">Tech</span></a></strong>
         </p>
         <span>POS System</span>
     </div>
