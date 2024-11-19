8794117f152cb55495e28dd54c561f729c109712
jacky040 <116127513+jackysmith040@users.noreply.github.com>
2024-11-19 16:30:06 +0000

    add print functionality to sales.php

--- pos/sales.php
+++ pos/sales.php
@@ -5,6 +5,27 @@
         padding: 10px;
         cursor: pointer;
     }
+
+    @media print {
+        body * {
+            visibility: hidden;
+        }
+        .printableArea, .printableArea * {
+            visibility: visible;
+        }
+        .printableArea {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+        }
+        .card-header, .btn, .dataTables_wrapper {
+            display: none !important;
+        }
+        th:last-child, td:last-child {
+            display: none !important; /* Remove Actions column */
+        }
+    }
 </style>
 
 <div class="container-fluid">
@@ -18,20 +39,21 @@
                     <div class="card-header">
                         <b>Sales Summary</b>
                         <span class="float:right">
-                            <a class="btn btn-primary btn-sm col-sm-2 float-right" href="billing/index.php"
-                                id="new_order">
+                            <a class="btn btn-primary btn-sm col-sm-2 float-right" href="billing/index.php" id="new_order">
                                 <i class="fa fa-plus"></i> New Sale
                             </a>
+                            <button class="btn btn-success btn-sm col-sm-2 float-right mr-2" onclick="printTable()">
+                                <i class="fa fa-print"></i> Print
+                            </button>
                         </span>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body printableArea">
                         <table class="table table-hover">
                             <thead>
                                 <tr>
                                     <th>#</th>
                                     <th>Date</th>
                                     <th>Invoice</th>
-                                    <!-- <th>Items</th> New Column for Item Details -->
                                     <th>Amount</th>
                                     <th>Payment Method</th>
                                     <th>Actions</th>
@@ -42,13 +64,6 @@
                                 $i = 1;
                                 $sales = $conn->query("SELECT * FROM sales ORDER BY unix_timestamp(date_created) DESC");
                                 while ($row = $sales->fetch_assoc()):
-                                    // Decode JSON details for items
-                                    $items = json_decode($row['details'], true);
-                                    $item_summary = "";
-                                    foreach ($items as $item) {
-                                        $item_summary .= $item['name'] . " (" . $item['quantity'] . "), ";
-                                    }
-                                    $item_summary = rtrim($item_summary, ", ");
                                     ?>
                                     <tr>
                                         <td class="text-center"><?php echo $i++ ?></td>
@@ -58,9 +73,6 @@
                                         <td>
                                             <p><?php echo $row['ref_no'] ?></p>
                                         </td>
-                                        <!-- <td>
-                                            <p><?php echo $item_summary ?></p>
-                                        </td> Display Items -->
                                         <td>
                                             <p class="text-right"><?php echo number_format($row['total_amount'], 2) ?></p>
                                         </td>
@@ -68,16 +80,14 @@
                                             <p><?php echo $row['payment_method'] ?></p>
                                         </td>
                                         <td class="text-center">
-                                            <button class="btn btn-sm btn-info view_items" type="button"
-                                                data-id="<?php echo $row['id'] ?>">
+                                            <button class="btn btn-sm btn-info view_items" type="button" data-id="<?php echo $row['id'] ?>">
                                                 <i class="fa fa-eye"></i> View
                                             </button>
-                                            
-                                            <?php if($_SESSION['login_type'] == 1):?>
-                                            <button class="btn btn-sm btn-danger delete_order" type="button"
-                                                data-id="<?php echo $row['id'] ?>"><i class="fa fa-trash-alt"></i>
-                                                Delete</button>
-                                            <?php endif;?>
+                                            <?php if ($_SESSION['login_type'] == 1): ?>
+                                            <button class="btn btn-sm btn-danger delete_order" type="button" data-id="<?php echo $row['id'] ?>">
+                                                <i class="fa fa-trash-alt"></i> Delete
+                                            </button>
+                                            <?php endif; ?>
                                         </td>
                                     </tr>
                                 <?php endwhile; ?>
@@ -102,7 +112,7 @@
 
 <script>
     $(document).ready(function () {
-        $('table').dataTable()
+        $('table').dataTable();
     });
 
     $('.view_items').click(function () {
@@ -110,23 +120,42 @@
     });
 
     $('.delete_order').click(function () {
-        _conf("Are you sure to delete this order?", "delete_order", [$(this).attr('data-id')])
+        _conf("Are you sure to delete this order?", "delete_order", [$(this).attr('data-id')]);
     });
 
     function delete_order(id) {
-        start_load()
+        start_load();
         $.ajax({
             url: 'ajax.php?action=delete_order',
             method: 'POST',
             data: { id: id },
             success: function (resp) {
                 if (resp == 1) {
-                    alert_toast("Data successfully deleted", 'success')
+                    alert_toast("Data successfully deleted", 'success');
                     setTimeout(function () {
-                        location.reload()
-                    }, 1500)
+                        location.reload();
+                    }, 1500);
                 }
             }
-        })
+        });
     }
+
+    function printTable() {
+    // Disable DataTable features for print
+    $('table').DataTable().destroy();
+
+    // Trigger the print dialog
+    window.print();
+
+    // Auto-refresh after print
+    window.onafterprint = function () {
+        location.reload();
+    };
+
+    // Auto-refresh when the window is closed
+    window.addEventListener('beforeunload', function () {
+        location.reload();
+    });
+}
+
 </script>
